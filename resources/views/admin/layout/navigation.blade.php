77e5789145ffb6fe2b180d1f7cf5d5a01f3d54c2
Yanik Kumar <believemasters@gmail.com>
2020-03-22 20:23:31 +0530

    Added Permissions fucntionality

--- resources/views/admin/layout/navigation.blade.php
+++ resources/views/admin/layout/navigation.blade.php
@@ -12,7 +12,10 @@
             <div id="main-menu" class="main-menu collapse navbar-collapse">
                 <ul class="nav navbar-nav">
                     <li class="active">
-                        <a href="index.html"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
+                        <a href="{{ url('/admin/dashboard') }}"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
+                    </li>
+                    <li>
+                        <a href="{{ url('/admin/permission') }}"> <i class="menu-icon ti-lock"></i>Permissions </a>
                     </li>
                     <h3 class="menu-title">UI elements</h3><!-- /.menu-title -->
                     <li class="menu-item-has-children dropdown">
